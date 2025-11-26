e8d94ff1d6660edcf65863e50202959e264a9815
FBI-Open-Upp <arturbreus08@gmail.com>
2025-11-26 20:57:07 +0100

    modal window

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,8 @@
-// Get the modal
-var modal = document.getElementById('beackdrop');
+const modal = document.querySelector('.beackdrop');
+const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
 
-// Get the button that opens the modal
-var btn = document.getElementById('modal-btn-open');
+const toggleModal = () => modal.classList.toggle('is-hidden');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('modal-btn-close')[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
